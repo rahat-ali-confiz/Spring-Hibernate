ab05372dd47e7217628c8715d8ad49122da4c029
rahat.ali <0010Zarnab786Asg!>
2022-10-07 18:22:46 +0500

    AOP ordering of Aspects. Added threee new files for the ordering and created a separate file for pointcut expression

--- AOP/spring-demo-aop-pointcut-order-aspects/src/main/java/com/spring/aopdemo/aspect/MyDemoLoggingAspect.java
+++ AOP/spring-demo-aop-pointcut-order-aspects/src/main/java/com/spring/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,13 +2,15 @@ package com.spring.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
 
-    @Before("forDaoPackageNoGetterSetter()")
+    @Before("com.spring.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on method");
     }
