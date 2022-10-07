4eed01a15bcf1110fb0cd1d4808bb186c91964b6
rahat.ali <0010Zarnab786Asg!>
2022-10-07 17:16:27 +0500

    AOP pointcut deckaration changes

--- AOP/spring-demo-aop-pointcut-declarations-combo/src/main/java/com/spring/aopdemo/aspect/MyDemoLoggingAspect.java
+++ AOP/spring-demo-aop-pointcut-declarations-combo/src/main/java/com/spring/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -16,12 +16,26 @@ public class MyDemoLoggingAspect {
     @Pointcut("execution(* com.spring.aopdemo.dao.*.*(..))")
     private void forDaoPackage() {}
 
-    @Before("forDaoPackage()")
+    // create pointCut for getter methods
+    @Pointcut("execution(* com.spring.aopdemo.dao.*.get*(..))")
+    private void getter() {}
+
+    // create pointCut for setter methods
+    @Pointcut("execution(* com.spring.aopdemo.dao.*.set*(..))")
+    private void setter() {}
+
+    // create pointcut: include package ... exclude getter/setter
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGetterSetter() {}
+
+
+
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on method");
     }
 
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void performApiAnalytics() {
         System.out.println("\n=====>>> Performing API analytics");
     }
