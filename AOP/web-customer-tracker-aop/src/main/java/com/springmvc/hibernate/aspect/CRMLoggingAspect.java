f0998b621d271dedf75d5a89874e0614f9b9cd52
rahat.ali <0010Zarnab786Asg!>
2022-11-21 16:48:44 +0500

    Spring Security Code

---
+++ AOP/web-customer-tracker-aop/src/main/java/com/springmvc/hibernate/aspect/CRMLoggingAspect.java
@@ -0,0 +1,82 @@
+package com.springmvc.hibernate.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.hibernate.sql.JoinType;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.JoinTable;
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+    // setup aspect
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+
+    // setup pointcut declarations
+    @Pointcut("execution(* com.springmvc.hibernate.controller.*.*(..))")
+    private void forControllerPackage(){
+
+    }
+
+    // do the same for service and dao
+    @Pointcut("execution(* com.springmvc.hibernate.service.*.*(..))")
+    private void forServicePackage(){
+
+    }
+
+    @Pointcut("execution(* com.springmvc.hibernate.dao.*.*(..))")
+    private void forDaoPackage(){
+
+    }
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow() {}
+
+    // add @Before advice
+    @Before("forAppFlow()")
+    public void before(JoinPoint joinPoint){
+
+        // display method we are calling
+        String theMethod = joinPoint.getSignature().toShortString();
+        myLogger.info("=======>> in @Before: calling method: " + theMethod);
+
+        // display the arguments to the method
+
+
+        // get the arguments
+        Object[] args = joinPoint.getArgs();
+
+        // loop thru and display args
+        for (Object tempArg : args){
+            myLogger.info("=========> arguments: " + tempArg);
+        }
+
+    }
+
+    // add @AfterReturning advice
+
+    @AfterReturning(
+            pointcut = "forAppFlow()",
+            returning = "theResult"
+    )
+    public void afterReturning(JoinPoint joinPoint, Object theResult){
+
+        // dis[lay method we are returning from
+        String theMethod = joinPoint.getSignature().toShortString();
+        myLogger.info("=======>> in @AfterReturning: calling method: " + theMethod);
+
+
+        // display data returned
+        myLogger.info("=========>> result: " + theResult);
+
+    }
+
+
+
+}
---
+++ AOP/web-customer-tracker-aop/src/main/java/com/springmvc/hibernate/service/CustomerServiceImpl.java
@@ -0,0 +1,52 @@
+package com.springmvc.hibernate.service;
+
+import com.springmvc.hibernate.dao.CustomerDAO;
+import com.springmvc.hibernate.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    // Need to inject the customer DAO.
+
+    @Autowired
+    private CustomerDAO customerDAO;
+
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        return customerDAO.getCustomers();
+    }
+
+    @Override
+    @Transactional
+    public void saveCustomer(Customer theCustomer) {
+
+        customerDAO.saveCustomer(theCustomer);
+    }
+
+    @Override
+    @Transactional
+    public Customer getCustomers(int theId) {
+        return customerDAO.getCustomers(theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCustomer(int theId) {
+         customerDAO.deleteCustomer(theId);
+    }
+
+    @Override
+    @Transactional
+    public List<Customer> searchCustomers(String theSearchName) {
+        return customerDAO.searchCustomers(theSearchName);
+    }
+
+
+}
